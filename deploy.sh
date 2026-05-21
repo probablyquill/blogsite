0c4be53745aba9c40f8a4235201a0677bc5b553d
probablyquill <elijah.tate@icloud.com>
2026-05-20 22:59:29 -0400

    Fixed deployment script
    Fixed the deployment script (to use gzip compression, need to use cp instead of rsync.

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ cd /home/elijah/Documents/GitHub/blogsite/
 hugo
 
 # Copy the contents of the public folder to the Cloud Storage Bucket
-gcloud storage rsync public gs://blog.probablyquill.com/ --cache-control max-age=0 --recursive --content-language en --gzip-local
+gcloud storage cp -Z public gs://blog.probablyquill.com/ --cache-control max-age=0 --recursive --content-language en
